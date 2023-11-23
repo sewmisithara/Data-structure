f1ae17bd6f18cfe4a44e7406b8fbda5566dc795f
Sewmi Sithara <121851351+sewmisithara@users.noreply.github.com>
2023-11-23 09:53:01 +0530

    Initial commit

---
+++ src/stack/Main.java
@@ -0,0 +1,25 @@
+package stack;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Peeked: " + stack.peek());
+
+        stack.printStack();
+
+        System.out.println("Popped: " + stack.pop());
+        stack.printStack();
+
+        System.out.println("Popped: " + stack.pop());
+        stack.printStack();
+
+        System.out.println("Peeked: " + stack.peek());
+    }
+}
---
+++ src/stack/Stack.java
@@ -0,0 +1,32 @@
+package stack;
+
+
+public class Stack {
+    private int[] elementData;
+    private int top;    // points to the top of the stack(last element)
+
+    public Stack(final int initialCapacity) {
+        elementData = new int[initialCapacity];
+        top = -1;
+    }
+
+    public void push(int data) {
+        elementData[++top] = data;
+    }
+
+    public int pop() {
+        return elementData[top--];
+    }
+
+    public int peek() { //top
+        return elementData[top];
+    }
+
+    public void printStack() {
+        System.out.print("[");
+        for (int i = 0; i <= top; i++) {
+            System.out.print(elementData[i] + ", ");
+        }
+        System.out.println("\b\b]");
+    }
+}
