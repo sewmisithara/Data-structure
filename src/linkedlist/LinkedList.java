2c9bd6f0946e6de94e4450aaf4b1b30631fb513a
Sewmi Sithara <121851351+sewmisithara@users.noreply.github.com>
2023-11-23 15:10:24 +0530

    crate LinkedList

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -2,6 +2,14 @@ package linkedlist;
 
 
 
+/*class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}*/
 public class LinkedList {
     private static class Node {
         private int data;
@@ -12,4 +20,118 @@ public class LinkedList {
         }
     }
     private Node head;
+
+    public void insertAtBeginning(int data) {
+        Node node = new Node(data);
+
+        node.next = head;
+        head = node;
+    }
+
+    public void insertAtEnding(int data) {
+        if (head == null) {
+            insertAtBeginning(data);
+            return;
+        }
+
+        Node node = new Node(data);
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = node;
+    }
+
+    public void insertAtPosition(int data, int position) {
+        if (position == 0) {
+            insertAtBeginning(data);
+            return;
+        }
+        Node node = new Node(data);
+
+        Node temp = head;
+        for (int i = 0; i < position - 1; i++) {
+            temp = temp.next;
+        }
+        node.next = temp.next;
+        temp.next = node;
+    }
+
+    public void deleteAtBeginning() {
+        if (head == null) {
+            System.err.println("List is empty");
+            return;
+        }
+        head = head.next;
+    }
+
+    public void deleteAtEnding() {
+        if(head == null) {
+            System.err.println("List is empty");
+            return;
+        }
+        Node temp = head;
+
+        while(temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+    }
+
+    public void deleteAtPosition(int position) {
+        if(head == null) {
+            System.err.println("List is empty");
+            return;
+        }
+
+        if(position == 0) {
+            deleteAtBeginning();
+            return;
+        }
+
+        Node temp = head;
+        for (int i = 0; i < position - 1; i++) {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+    }
+
+    public int size() {
+        int count = 0;
+        Node temp = head;
+
+        while(temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public void clear() {
+        head = null;
+    }
+
+    public int get(int position) {
+        if(head == null) {
+            System.err.println("List is empty");
+            return -1;
+        }
+
+        Node temp = head;
+        for (int i = 0; i < position; i++) {
+            temp = temp.next;
+        }
+        return temp.data;
+    }
+
+    public void traverse() {
+        System.out.print("[");
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + ", ");
+            temp = temp.next;
+        }
+        System.out.println(head == null ? "Empty list]" : "\b\b]");
+    }
 }
--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -4,6 +4,38 @@ package linkedlist;
 
 public class Main {
     public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
 
+        ll.insertAtBeginning(10);
+        ll.insertAtBeginning(20);
+        ll.insertAtBeginning(30);
+
+        ll.insertAtEnding(40);
+
+        ll.traverse();
+
+        ll.insertAtPosition(25, 2);
+        ll.traverse(); //
+
+        ll.deleteAtBeginning();
+        ll.traverse();
+
+        ll.deleteAtEnding();
+        ll.traverse(); //[20, 25, 40]
+
+        ll.deleteAtPosition(1);
+        ll.traverse(); //[20, 10]
+
+        System.out.println("size: " + ll.size());
+
+        ll.insertAtEnding(100);
+
+        ll.traverse();  //[20, 10, 100]
+
+        int data = ll.get(0);
+        System.out.println("data: " + data); //10
+
+        ll.clear();
+        ll.traverse(); //[Empty list]
     }
 }
